6477a9d7ed7b641a386841842fbb08ee86773a59
Caio Goes <caiocezar23042001@gmail.com>
2024-09-28 19:35:53 -0300

    Adicionado função de coleta de dados

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def main():
     print(2*2)
     return None
+def sum(a, b):
+    a = int(input("Numero 1:"))
+    b = int(input("Numero 2:"))
+    return a,b
 if __name__ == "__main__":
     main()
     
