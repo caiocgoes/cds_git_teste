48fd1571d6bcfe414e629624a8e792d6d0263b01
Caio Goes <caiocezar23042001@gmail.com>
2024-09-28 19:38:21 -0300

    Adicionado função de divisão

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-def main():
-    print(2*2)
-    return None
 def sum(a, b):
     a = int(input("Numero 1:"))
     b = int(input("Numero 2:"))
     return a,b
+def main():
+    n1,n2 = sum()
+    print(n1+n2)
+def div():
+    n1,n2 = sum()
+    print(n1/n2)
 if __name__ == "__main__":
     main()
     
